936ea5eb21f107d16dbdd48eb7cc82b5f76617ce
Matt Erickson <peawee@peawee.net>
2015-03-06 08:43:32 -0600

    added some documentation to chatcontrol_nicklist

--- Floe.UI/ChatControl/ChatControl_NickList.cs
+++ Floe.UI/ChatControl/ChatControl_NickList.cs
@@ -7,78 +7,82 @@ using Floe.Net;
 
 namespace Floe.UI
 {
-	public partial class ChatControl : ChatPage
-	{
-		private string[] _nickCandidates;
-		private NicknameList _nickList;
+    public partial class ChatControl : ChatPage
+    {
+        private string[] _nickCandidates;
+        private NicknameList _nickList;
 
-		public NicknameList Nicknames
-		{
-			get { return _nickList; }
-		}
-		
-		private string GetNickWithLevel(string nick)
-		{
-			return this.IsChannel && _nickList.Contains(nick) ? _nickList[nick].ToString() : nick;
-		}
+        public NicknameList Nicknames
+        {
+            get { return _nickList; }
+        }
+        
+        private string GetNickWithLevel(string nick)
+        {
+            return this.IsChannel && _nickList.Contains(nick) ? _nickList[nick].ToString() : nick;
+        }
 
-		private string GetNickWithoutLevel(string nick)
-		{
-			return (nick.Length > 1 && (nick[0] == '@' || nick[0] == '+' || nick[0] == '%')) ? nick.Substring(1) : nick;
-		}
+        private string GetNickWithoutLevel(string nick)
+        {
+            return (nick.Length > 1 && (nick[0] == '@' || nick[0] == '+' || nick[0] == '%')) ? nick.Substring(1) : nick;
+        }
 
-		private void DoNickCompletion()
-		{
-			int start = 0, end = 0;
-			if (txtInput.Text.Length > 0)
-			{
-				start = Math.Max(0, txtInput.CaretIndex - 1);
-				end = start < txtInput.Text.Length ? start + 1 : start;
+        private void DoNickCompletion()
+        {
+            int start = 0, end = 0;
+            if (txtInput.Text.Length > 0)
+            {
+                // If we get here, we're going to use start and end to define a selection of a possible nickname fragment
+                // in the txtInput field, centered around the input caret.
+                start = Math.Max(0, txtInput.CaretIndex - 1);
+                end = start < txtInput.Text.Length ? start + 1 : start;
 
-				while (start >= 0 && NicknameItem.IsNickChar(txtInput.Text[start]))
-				{
-					start--;
-				}
-				start++;
+                // Hunt backwards from start, testing each character in the text input for valid IRC nick characters.
+                while (start >= 0 && NicknameItem.IsNickChar(txtInput.Text[start]))
+                {
+                    start--;
+                }
+                start++;
 
-				while (end < txtInput.Text.Length && NicknameItem.IsNickChar(txtInput.Text[end]))
-				{
-					end++;
-				}
-			}
-			else
-			{
-				start = end = 0;
-			}
+                // Hunt forwards from end, testing each character in the text input for valid IRC nick characters.
+                while (end < txtInput.Text.Length && NicknameItem.IsNickChar(txtInput.Text[end]))
+                {
+                    end++;
+                }
+            }
+            else
+            {
+                start = end = 0;
+            }
 
-			if (end - start > 0)
-			{
-				string nickPart = txtInput.Text.Substring(start, end - start);
-				string nextNick = null;
-				if (_nickCandidates == null)
-				{
-					_nickCandidates = (from n in this.Nicknames
-									   where n.Nickname.StartsWith(nickPart, StringComparison.InvariantCultureIgnoreCase)
-									   orderby n.Nickname.ToLowerInvariant()
-									   select n.Nickname).ToArray();
-					if (_nickCandidates.Length > 0)
-					{
-						nextNick = _nickCandidates[0];
-					}
-				}
+            if (end - start > 0)
+            {
+                string nickPart = txtInput.Text.Substring(start, end - start);
+                string nextNick = null;
+                if (_nickCandidates == null)
+                {
+                    _nickCandidates = (from n in this.Nicknames
+                                       where n.Nickname.StartsWith(nickPart, StringComparison.InvariantCultureIgnoreCase)
+                                       orderby n.Nickname.ToLowerInvariant()
+                                       select n.Nickname).ToArray();
+                    if (_nickCandidates.Length > 0)
+                    {
+                        nextNick = _nickCandidates[0];
+                    }
+                }
 
-				for (int i = 0; i < _nickCandidates.Length; i++)
-				{
-					if (string.Compare(_nickCandidates[i], nickPart, StringComparison.InvariantCulture) == 0)
-					{
-						nextNick = i < _nickCandidates.Length - 1 ? _nickCandidates[i + 1] : _nickCandidates[0];
-						break;
-					}
-				}
+                for (int i = 0; i < _nickCandidates.Length; i++)
+                {
+                    if (string.Compare(_nickCandidates[i], nickPart, StringComparison.InvariantCulture) == 0)
+                    {
+                        nextNick = i < _nickCandidates.Length - 1 ? _nickCandidates[i + 1] : _nickCandidates[0];
+                        break;
+                    }
+                }
 
-				var keepNickCandidates = _nickCandidates;
-				if (nextNick != null)
-				{
+                var keepNickCandidates = _nickCandidates;
+                if (nextNick != null)
+                {
                     if (!txtInput.Text.Contains(' '))
                     {
                         nextNick += ": ";
@@ -87,11 +91,11 @@ namespace Floe.UI
                     {
                         nextNick += " ";
                     }
-					txtInput.Text = txtInput.Text.Substring(0, start) + nextNick + txtInput.Text.Substring(end);
-					txtInput.CaretIndex = start + nextNick.Length;
-				}
-				_nickCandidates = keepNickCandidates;
-			}
-		}
-	}
+                    txtInput.Text = txtInput.Text.Substring(0, start) + nextNick + txtInput.Text.Substring(end);
+                    txtInput.CaretIndex = start + nextNick.Length;
+                }
+                _nickCandidates = keepNickCandidates;
+            }
+        }
+    }
 }
